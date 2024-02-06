2d2f6558ff625e175df1519e0e53c5a606fd92a1
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2024-02-06 13:42:45 +0100

    add refs

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,46 @@ import NewProject from './components/NewProject'
 import NoProjectSelected from './components/NoProjectSelected.jsx'
 
 function App() {
-	// Project object =>
-	// const project = [
-	// 	{ title: 'something', data: 'date', tasks: [] },
-	// 	{ title: 'coś', data: 'ss', tasks: [] },
-	// ]
+	const [projectState, setProjectState] = useState({
+		selectedProjectId: undefined,
+		projects: [],
+	})
+
+	function handleStartAddProject() {
+		setProjectState(prevState => {
+			return {
+				...prevState,
+				selectedProjectId: null,
+			}
+		})
+	}
+	let content
+
+	if (projectState.selectedProjectId === null) {
+		content = <NewProject />
+	} else if (projectState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+	}
+
+	function onAdd(projectData) {
+		setProjectState(
+			...prevState => {
+				const newProject = {
+					...projectData,
+					id: Math.random(),
+				}
+				return {
+					...prevState,
+					projects: [...prevState.projects, newProject],
+				}
+			}
+		)
+	}
 
 	return (
 		<main className="h-screen my-8 flex gap-8">
-			<ProjectSideBar />
-			<NoProjectSelected />
+			<ProjectSideBar onStartAddProject={handleStartAddProject} />
+			{content}
 		</main>
 	)
 }
--- src/components/ProjectSideBar..jsx
+++ src/components/ProjectSideBar..jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import Button from './Button'
 
-export default function ProjectSideBar() {
+export default function ProjectSideBar({ onStartAddProject }) {
 	return (
 		<aside className="w-1/4 px-8 py-16 bg-stone-900 text-stone-50 rounded-r-md">
 			<h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
 			<div>
-				<Button>+Add new project</Button>
+				<Button onClick={onStartAddProject}>+Add new project</Button>
 			</div>
 			<ul>...</ul>
 		</aside>
